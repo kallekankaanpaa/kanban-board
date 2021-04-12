218dd9faaaae92053c27660c448b321c89a6d5ff
Kalle Kankaanpää <kalle.kankaanpaa@aalto.fi>
2021-04-12 14:23:14 +0300

    Added file save and load methods to Utils

--- src/main/scala/ui/Utils.scala
+++ src/main/scala/ui/Utils.scala
@@ -2,8 +2,9 @@ package ui
 
 import scalafxml.core.{ControllerDependencyResolver, FXMLView, NoDependencyResolver}
 import scalafx.Includes._
-import java.io.IOException
+import java.io.{IOException, ObjectOutputStream, ObjectInputStream, FileOutputStream, FileInputStream}
 import scalafx.scene.Parent
+import data.Board
 
 object Utils {
   def readFXML(path: String, controller: ControllerDependencyResolver): Parent = {
@@ -13,4 +14,28 @@ object Utils {
   }
 
   def readFXML(path: String): Parent = readFXML(path, NoDependencyResolver)
+
+  def save(board: Board): Unit = {
+    val stream = new ObjectOutputStream(new FileOutputStream(filename(board.name)))
+    stream.writeObject(board)
+    stream.close
+  }
+
+  def load(path: String): Board = {
+    val stream = new ObjectInputStream(new FileInputStream(path))
+    val data = stream.readObject
+    stream.close
+    data.asInstanceOf[Board]
+  }
+
+  /** Generates filename from parameter.
+    *
+    * Changes all characters to lowercase first since some filesystems are case-sensitive.
+    * Replaces all whitespace characters with underscores and append .kbb (KanBan Board)
+    * suffix to the filename
+    *
+    * @param name Original name to turn into filename
+    * @return Filesystem friendly name for the file
+    */
+  private def filename(name: String): String = name.toLowerCase.replaceAll("\\s", "_") + ".kbb"
 }
