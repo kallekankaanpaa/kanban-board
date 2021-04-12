28345780f220c00439fdd2da63e3b3ed447ccaea
Kalle Kankaanpää <kalle.kankaanpaa@aalto.fi>
2021-04-12 13:10:19 +0300

    Added Serializable trait to Column

--- src/main/scala/data/Column.scala
+++ src/main/scala/data/Column.scala
@@ -5,7 +5,8 @@ import scalafx.scene.Parent
 import scala.reflect.runtime.universe.typeOf
 import scalafxml.core.DependenciesByType
 
-class Column(val name: String, val cards: Set[Card]) extends Component {
+@SerialVersionUID(1L)
+class Column(val name: String, val cards: Set[Card]) extends Serializable with Component {
   val fxmlPath: String = "/fxml/Column.fxml"
   def toUIComponent: Parent =
     Utils.readFXML(fxmlPath, new DependenciesByType(Map(typeOf[Set[Card]] -> cards, typeOf[String] -> name)))
